3718c33ff9f52cfa7206ff87d31bbc76ffa55bb2
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-25 17:32:17 -0300

    refactor: component Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,20 @@ import {
   useRef,
   useState,
   useCallback,
+  HTMLAttributes,
+  forwardRef
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  name: string
+}
+
+
+const Input = ({ name, ...rest }: InputProps) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +31,8 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    // setIsFilled(!!inputRef.current?.value);
+    setIsFilled(true);
   }, []);
 
   useEffect(() => {
@@ -37,8 +45,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
-
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
@@ -50,4 +56,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   );
 };
 
-export default Input;
+export default forwardRef(Input);
